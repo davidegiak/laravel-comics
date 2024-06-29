4a5b75b56ac98318a28005c42804b4281203d88a
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-06-29 12:07:55 +0200

    milestone 3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>laravel-comics</title>
+    <link rel="stylesheet" href="app.css">
 </head>
 <body>
-    @foreach ($msgs as $item)
-        <h1>{{$item}} </h1>
-    @endforeach
+    <div class="container">
+        @foreach ($comics as $comic)
+        <div class="card">
+            <img src=" {{$comic['thumb']}} " alt="">
+            <h4>{{$comic['title']}} </h4>
+            <p>{{$comic['description']}} </p>
+            <span> {{$comic['series']}} </span>
+            <span> {{$comic['sale_date']}} </span>
+            <span> {{$comic['type']}} </span>
+        </div>
+        @endforeach
+    </div>
 </body>
 </html>
