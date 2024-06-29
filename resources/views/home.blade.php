d692917c0299149243a8d3146c31d6ac641e103e
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-06-29 13:05:18 +0200

    FINE

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,18 +8,30 @@
     <title>laravel-comics</title>
     <link rel="stylesheet" href="app.css">
 </head>
+
 <body>
     <div class="container">
         @foreach ($comics as $comic)
-        <div class="card">
-            <img src=" {{$comic['thumb']}} " alt="">
-            <h4>{{$comic['title']}} </h4>
-            <p>{{$comic['description']}} </p>
-            <span> {{$comic['series']}} </span>
-            <span> {{$comic['sale_date']}} </span>
-            <span> {{$comic['type']}} </span>
-        </div>
+            <div class="card">
+                <div class="desc-card">
+                    <h4>{{ $comic['title'] }} </h4>
+                    <p>{{ $comic['description'] }} </p>
+                    <span> {{ $comic['series'] }} </span>
+                    <span> {{ $comic['sale_date'] }} </span>
+                    <span> {{ $comic['type'] }} </span>
+                </div>
+                <div class="face">
+                    <img src=" {{ $comic['thumb'] }} " alt="">
+                    <h4>{{ $comic['title'] }} </h4>
+                    <p class="description">{{ $comic['description'] }} </p>
+                    <span class="description"> {{ $comic['series'] }} </span>
+                    <span class="description"> {{ $comic['sale_date'] }} </span>
+                    <span class="description"> {{ $comic['type'] }} </span>
+                </div>
+            </div>
         @endforeach
     </div>
+    @include('about')
 </body>
+
 </html>
